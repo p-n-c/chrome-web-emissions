f6832bba20b0c4455805bda11f4473531b123320
Daniel Hartley <daniel.hartley@altruistiq.com>
2024-09-10 19:50:05 +0100

    revert simple working version

--- publish/service-worker.js
+++ publish/service-worker.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-undef */
-// import { getPageEmissions } from './emissions-script'
 
 // Open the panel when the visitor clicks on the extension icon
 chrome.action.onClicked.addListener((tab) => {
   chrome.sidePanel.open({ tabId: tab.id })
   chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true })
+  console.log('tab: ', tab)
 })
 
 chrome.runtime.onConnect.addListener((port) => {
@@ -18,10 +18,3 @@ chrome.runtime.onConnect.addListener((port) => {
     })
   }
 })
-;(async () => {
-  const [tab] = await chrome.tabs.query({
-    active: true,
-    lastFocusedWindow: true,
-  })
-  console.log(tab)
-})()
