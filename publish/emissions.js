008ac69749dbdb5f8bd95861d3568d032e2ad272
Daniel Hartley <daniel.hartley@altruistiq.com>
2024-09-12 14:55:49 +0100

    add compressionRates

--- publish/emissions.js
+++ publish/emissions.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef */
 const registerServiceWorker = () => {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
@@ -22,6 +21,100 @@ const registerServiceWorker = () => {
 // db constants
 const DB = 'emissionsDB'
 const STORE = 'emissions'
+const compressionRates = {
+  brotli: [
+    {
+      level: 0,
+      rate: 1.2,
+    },
+    {
+      level: 1,
+      rate: 2.1,
+    },
+    {
+      level: 2,
+      rate: 2.5,
+    },
+    {
+      level: 3,
+      rate: 3.0,
+    },
+    {
+      level: 4,
+      rate: 3.5,
+    },
+    {
+      level: 5,
+      rate: 4.0,
+    },
+    {
+      level: 6,
+      rate: 4.3,
+    },
+    {
+      level: 7,
+      rate: 4.6,
+    },
+    {
+      level: 8,
+      rate: 4.9,
+    },
+    {
+      level: 9,
+      rate: 5.0,
+    },
+    {
+      level: 10,
+      rate: 5.1,
+    },
+    {
+      level: 11,
+      rate: 5.2,
+    },
+  ],
+  gzip: [
+    {
+      level: 0,
+      rate: 1,
+    },
+    {
+      level: 1,
+      rate: 1.5,
+    },
+    {
+      level: 2,
+      rate: 2.0,
+    },
+    {
+      level: 3,
+      rate: 2.3,
+    },
+    {
+      level: 4,
+      rate: 2.5,
+    },
+    {
+      level: 5,
+      rate: 2.8,
+    },
+    {
+      level: 6,
+      rate: 3.0,
+    },
+    {
+      level: 7,
+      rate: 3.2,
+    },
+    {
+      level: 8,
+      rate: 3.3,
+    },
+    {
+      level: 9,
+      rate: 3.4,
+    },
+  ],
+}
 
 const CO2_PER_KWH_IN_DC_GREY = 519
 const CO2_PER_KWH_NETWORK_GREY = 475
